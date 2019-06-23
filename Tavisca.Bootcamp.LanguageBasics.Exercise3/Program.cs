6e88d2d5597551cfe61df7b87d7e89708a1379f2
tavisca-sniyazi <sniyazi@tavisca.in>
2019-06-24 03:38:06 +0530

    Update Program.cs

--- Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
@@ -38,10 +39,94 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             Console.WriteLine(result);
         }
 
+        
+        public static int Answers(string dietPlan, int[] protein, int[] carbs, int[] fat, int[] calories)
+        {
+            
+            List<int> item = new List<int>();
+            for (int i = 0; i < protein.Length; i++)
+            {
+                item.Add(i);
+            }
+            int y;
+            foreach (var x in dietPlan)
+            {
+                switch (x)
+                {
+                    case 'P':
+                        y = Max(protein, item);
+                        item = indexs(protein, item, y); break;
+                    case 'p':
+                        y = Min(protein, item);
+                        item = indexs(protein, item, y); break;
+                    case 'C':
+                        y = Max(carbs, item);
+                        item = indexs(carbs, item, y); break;
+                    case 'c':
+                        y = Min(carbs, item);
+                        item = indexs(carbs, item, y); break;
+                    case 'F':
+                        y = Max(fat, item);
+                        item = indexs(fat, item, y); break;
+                    case 'f':
+                        y = Min(fat, item);
+                        item = indexs(fat, item, y); break;
+                    case 'T':
+                        y = Max(calories, item);
+                        item = indexs(calories, item, y); break;
+                    case 't':
+                        y = Min(calories, item);
+                        item = indexs(calories, item, y); break;
+                }
+            }
+            return item[0];
+        }
+        
+        public static List<int> indexs(int[] arr, List<int> item, int y)
+        {
+            List<int> temp = new List<int>();
+            foreach (var i in item)
+            {
+                if (arr[i] == y)
+                    temp.Add(i);
+            }
+            return temp;
+        }
+        
+       
+        
+        private static int Min(int[] arr, List<int> item)
+        {
+            int min = arr[item[0]];
+            for (int i = 1; i < item.Count; i++)
+            {
+                if (arr[item[i]] < min)
+                    min = arr[item[i]];
+            }
+            return min;
+        }
+
+        private static int Max(int[] arr, List<int> item)
+        {
+            int max = arr[item[0]];
+            for (int i = 1; i < item.Count; i++)
+            {
+                if (arr[item[i]] > max)
+                    max = arr[item[i]];
+            }
+            return max;
+        }
         public static int[] SelectMeals(int[] protein, int[] carbs, int[] fat, string[] dietPlans)
         {
-            // Add your code here.
-            throw new NotImplementedException();
+            
+            int[] calories = new int[protein.Length];
+            int[] ans = new int[dietPlans.Length];
+            for (int i = 0; i < protein.Length; i++)
+                calories[i] = ((protein[i])*5 )+ ((carbs[i]) * 5) + fat[i] * 9;
+            for (int i = 0; i < dietPlans.Length; i++)
+                ans[i] = Answers(dietPlans[i], protein, carbs, fat, calories);
+            return ans;
+            
         }
     }
 }
